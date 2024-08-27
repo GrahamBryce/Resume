ca9a1a1867e2c03e67b4debd48a54b8d7041f4c8
Bryce Graham <bgraham1@student.fullsail.edu>
2024-08-27 16:48:55 -0500

    Working front end and back end

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,39 @@
-const express = require("express");
-// const morgan = require("morgan");
-// const cors = require("cors");
+const express = require('express');
+const cors = require('cors'); 
 require('dotenv').config();
-const app = require("./index")
 
-// Use environment variable for the port, or default to 3000
-const port = process.env.PORT || 3000;
 
-// Will move this to server.js
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Your routes
+const emailRoutes = require('./routes/emailRoutes');
+app.use('/api', emailRoutes);
+
+// Start the server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
+// const express = require('express');
+// const cors = require('cors'); // Import cors middleware
+
+// const app = express();
+
+// // Use CORS middleware
+// app.use(cors());
+
+// // Other middleware (e.g., body parsing)
+// app.use(express.json());
+
+// // Your routes
+// const emailRoutes = require('./routes/emailRoutes');
+// app.use('/api', emailRoutes);
+
+// // Start the server
+// const PORT = process.env.PORT || 8000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
