d1d9d8c998aa3bd9beb15e7867b7552d83730f16
Bryce Graham <bgraham1@student.fullsail.edu>
2024-08-28 14:37:31 -0500

    fixed routes

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(cors({
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 // Your API routes here
-app.use('/api', require('./routes/apiRoutes')); // Example API route
+app.use('/api', require('./routes/emailRoutes')); // Example API route
 
 // The "catchall" handler: for any request that doesn't match an API route, send back React's index.html file
 app.get('*', (req, res) => {
@@ -24,6 +24,43 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+
+
+
+
+// this worked
+// const express = require('express');
+// const cors = require('cors');
+// const path = require('path');
+
+// const app = express();
+
+// // Enable CORS for your frontend
+// app.use(cors({
+//   origin: 'https://brycesresume-46f715e21449.herokuapp.com' // Your frontend's origin
+// }));
+
+// // Serve static files from the React app
+// app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// // Your API routes here
+// app.use('/api', require('./routes/apiRoutes')); // Example API route
+
+// // The "catchall" handler: for any request that doesn't match an API route, send back React's index.html file
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+// });
+
+// const PORT = process.env.PORT || 8000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+
+
+
 
 
 // const express = require('express');
