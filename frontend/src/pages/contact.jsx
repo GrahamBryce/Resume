9e2d3ce66f78a3c13ad549c967e47e32b6f86128
Bryce Graham <bgraham1@student.fullsail.edu>
2024-08-28 17:45:44 -0500

    updated name

--- frontend/src/pages/contact.jsx
+++ frontend/src/pages/contact.jsx
@@ -24,7 +24,7 @@ function Contact() {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://brycesresume-46f715e21449.herokuapp.com/api/send-email", {
+      const response = await fetch("https://b-graham.com/api/send-email", {
       // const response = await fetch("http://localhost:8000/api/send-email", {
         method: "POST",
         headers: {
